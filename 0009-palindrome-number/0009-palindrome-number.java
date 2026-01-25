e3fc2272c80ffc33e6a54b9fab1da94e5b41283b
Karthick <0105karthick@gmail.com>
2026-01-25 17:13:53 +0530

    Time: 5 ms (73.68%), Space: 46.2 MB (25.63%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -7,30 +7,18 @@ class Solution {
             return true;
         }
 
-        int size=(int) Math.floor((Math.log10(x)))+1;
+        int org=x;
+        int rev=0;
 
-        //System.out.println(size);
+        while(x>0){
+            rev=rev*10+x%10;
+            x=x/10;
 
-        for(int i=1;i<=size/2;i++){
-
-            // System.out.println(x/Math.pow(10,i-1));
-            // System.out.println(x%Math.pow(10,i-1));
-
-            int left= (int) (x/Math.pow(10,(size-i))) % 10;
-            int right = (int) (x%(Math.pow(10,i))/Math.pow(10,(i-1)));
-
-            System.out.println(left);
-            System.out.println(right);
-
-            if(left!=right){
-                return false;
-            }
-
-
-
-            
         }
-
+        if(rev!=org){
+            return false;
+        }
+        
 
         return true;
         
